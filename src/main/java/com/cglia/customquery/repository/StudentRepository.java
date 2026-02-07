c80467a46b924e74d66cc4b66b0e30843e7fc932
Rahul Gupta <rahuldevit2000@gmail.com>
2026-02-07 12:37:48 +0530

    Update StudentRepository.java
    Adding Student Repository

--- src/main/java/com/cglia/customquery/repository/StudentRepository.java
+++ src/main/java/com/cglia/customquery/repository/StudentRepository.java
@@ -1,13 +1,9 @@
 package com.cglia.customquery.repository;
-
 import java.util.Date;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.cglia.customquery.model.Student;
 
 @Repository
