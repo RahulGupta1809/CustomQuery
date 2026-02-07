784fd51cbd2974835369a0a9cc7b3caadb9e6bcf
Rahul Gupta <rahuldevit2000@gmail.com>
2026-02-07 12:35:42 +0530

    Update LoggingAspect.java
    Add Java Doc Comments

--- src/main/java/com/cglia/customquery/aspect/LoggingAspect.java
+++ src/main/java/com/cglia/customquery/aspect/LoggingAspect.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
+//Logging Class
 @Aspect
 @Component
 public class LoggingAspect {
@@ -33,3 +33,4 @@ public class LoggingAspect {
 		return result;
 	}
 }
+
