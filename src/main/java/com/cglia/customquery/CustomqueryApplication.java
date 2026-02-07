ca00a4d0d4e04b69903ee4b2242726b1c208515d
Rahul Gupta <rahuldevit2000@gmail.com>
2026-02-07 12:38:33 +0530

    Update CustomqueryApplication.java
    Custom Query added as a Java Doc

--- src/main/java/com/cglia/customquery/CustomqueryApplication.java
+++ src/main/java/com/cglia/customquery/CustomqueryApplication.java
@@ -1,11 +1,10 @@
-package com.cglia.customquery;
-
+package com.cglia.customquery;=
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//Custom Query main Class
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
