57d6f61d3fd3d779fb5d8585220cc85ee5db11bf
Rahul Gupta <rahuldevit2000@gmail.com>
2026-08-04 20:59:01 +0530

    Refactor StudentController for improved clarity

--- src/main/java/com/cglia/customquery/controller/StudentController.java
+++ src/main/java/com/cglia/customquery/controller/StudentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.cglia.customquery.model.Student;
 import com.cglia.customquery.service.StudentService;
-
+//Student Controller
 @RestController
 @RequestMapping("/api")
 public class StudentController {
